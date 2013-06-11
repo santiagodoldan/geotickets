fe1893e780549184ab25ac3b5e7d0015fb0d0105
Pablo Ifran <pabloifran@gmail.com>
2013-06-11 15:02:11 -0300

    pritify simple form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610220102) do
+ActiveRecord::Schema.define(:version => 20130610221238) do
 
   create_table "epics", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130610220102) do
     t.date     "end_date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "active"
   end
 
   create_table "tags", :force => true do |t|
