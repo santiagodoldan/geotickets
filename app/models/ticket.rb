9be30373a3a8145fb014a44e1d5c5ac38590e2d8
santiagodolte <santiagodolte@gmail.com>
2013-07-24 17:55:40 -0300

    Show correctly ticket name

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,20 +1,22 @@
 class Ticket < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :epic
 
   has_many :workers, class_name: 'User', through: :worked_hours, source: :user, uniq: true
   has_many :worked_hours
 
-  attr_accessible :description, :estimation, :name, :todo, :user_id, :epic_id, :system_ticket_id
+  validates :epic, :name, :estimation, presence: true
 
-  validates :epic, :name, :estimation, :presence => true
+  attr_accessible :description, :estimation, :name, :todo, :user_id, :epic_id, :system_ticket_id
 
   def to_s
     system_ticket_and_name
   end
 
   def system_ticket_and_name
-    "#{system_ticket_id} - #{name}"
+    return "#{system_ticket_id} - #{name}" if system_ticket_id.present?
+    name
   end
 
   def total_hours_by_tag
