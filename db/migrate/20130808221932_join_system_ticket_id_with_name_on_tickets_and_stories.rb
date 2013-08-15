10f53e9556af5ff0107ace846e71b7ce9e005445
santiagodolte <santiagodolte@gmail.com>
2013-08-15 01:30:10 -0300

    fix migration error

--- db/migrate/20130808221932_join_system_ticket_id_with_name_on_tickets_and_stories.rb
+++ db/migrate/20130808221932_join_system_ticket_id_with_name_on_tickets_and_stories.rb
@@ -1,11 +1,11 @@
 class JoinSystemTicketIdWithNameOnTicketsAndStories < ActiveRecord::Migration
   def up
     Ticket.scoped.each do |ticket|
-      ticket.update_column(name: "#{ticket.system_ticket_id} #{ticket.name}")
+      ticket.update_column(:name, "#{ticket.system_ticket_id} #{ticket.name}")
     end
 
     Story.scoped.each do |story|
-      story.update_column(name: "#{story.system_ticket_id} #{story.name}")
+      story.update_column(:name, "#{story.system_ticket_id} #{story.name}")
     end
   end
 
